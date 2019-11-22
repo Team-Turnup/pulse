4deeef41debabaafa044af7e1fb81d80f1cc876a
Munaiz <munaizahmed2@gmail.com>
2019-11-22 11:47:43 -0600

    finishing initial homescreen mockup

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,34 +7,67 @@ import {
   Header,
   Title,
   Content,
-  Accordion,
   Card,
   CardItem
 } from "native-base";
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryLabel,
+  VictoryAxis,
+  VictoryBrushLine
+} from "victory-native";
 
 export default class HomeScreen extends Component {
   render() {
     let dummyData = [
-      { name: "Beginner Workout", duration: "1 Hour", date: "Sept.15.2019" },
-      { name: "Intermediate Workout", duration: "1 Hour", date: "Oct.4.2019" },
-      { name: "Advanced Workout", duration: "1 Hour", date: "Nov.21.2019" },
-      { name: " Athlete Workout", duration: "1 Hour", date: "Dec.13.2019" }
+      { name: "First Workout", duration: 60, date: "Sept.15.2019" },
+      { name: "Second Workout", duration: 45, date: "Oct.4.2019" },
+      { name: "Third Workout", duration: 70, date: "Nov.21.2019" },
+      { name: "Fourth Workout", duration: 80, date: "Dec.13.2019" }
     ];
     return (
       <Container style={{ backgroundColor: "pink" }}>
-        {/* <Header style={{ backgroundColor: "blue" }}></Header> */}
-        <Content style={{ backgroundColor: "yellow" }}>
+        <Content style={{ backgroundColor: "midnightblue" }}>
           <Card>
             <CardItem header>
-              <Text>Recent Workouts</Text>
+              <Title>Recent Workouts</Title>
             </CardItem>
-            {dummyData.map(workout => {
+            {dummyData.map((workout, i) => {
               return (
-                <CardItem>
+                <CardItem
+                  button
+                  key={i}
+                  onPress={() =>
+                    alert(
+                      `Duration: ${workout.duration}\nDate: ${workout.date}`
+                    )
+                  }
+                >
                   <Text header>{workout.name}</Text>
                 </CardItem>
               );
             })}
+          </Card>
+          <Button block danger onPress={() => alert("New Workout Added")}>
+            <Text>Add New Workout</Text>
+          </Button>
+          <Card>
+            <VictoryChart domainPadding={5}>
+              <VictoryAxis dependentAxis tickFormat={x => `${x} min`} />
+              <VictoryAxis independentAxis tickFormat={y => y.split(' ')[0]} />
+              <VictoryLabel text="Recent Workouts" textAnchhor="middle"
+              x={225}
+              y={30}
+              />
+              <VictoryBar
+                style={{ data: { fill: "gold", width:20 } }}
+                alignment="start"
+                data={dummyData}
+                y={data => data.duration}
+                x={data => data.name}
+              />
+            </VictoryChart>
           </Card>
         </Content>
       </Container>
