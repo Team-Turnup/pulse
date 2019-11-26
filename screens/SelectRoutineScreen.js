56b6edaf5f0bc9348040a601957149352ea01e86
Dan Chun <dchun016@gmail.com>
2019-11-26 17:06:57 -0600

    finished SelectRoutineScreen and PreviousRoutine

--- screens/SelectRoutineScreen.js
+++ screens/SelectRoutineScreen.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import {
   Image,
   Platform,
   ScrollView,
   StyleSheet,
   TouchableOpacity,
-  View,
-} from 'react-native';
+  View
+} from 'react-native'
 import {
   Container,
   Header,
@@ -24,73 +24,81 @@ import {
   Body,
   Icon,
   Text,
-  CheckBox,
-  ListItem,
-} from 'native-base';
-import RNPickerSelect from 'react-native-picker-select';
-import PreviousRoutine from '../components/PreviousRoutine';
+  ListItem
+} from 'native-base'
+import RNPickerSelect from 'react-native-picker-select'
+import PreviousRoutine from '../screens/PreviousRoutine'
+import CheckBox from 'react-native-check-box'
 //import { getRoutineThunk } from '../store/routines';
-import { createRoutineThunk } from '../store/routines';
+import {createRoutineThunk} from '../store/routines'
+import activityTypes from '../assets/images/activityTypes'
 
 //maybe rename to CreateRoutineScreen
 class SelectRoutineScreen extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      selectedExerciseType: '',
-      hapticCheckbox: true,
-    };
-    this.onValueChange = this.onValueChange.bind(this);
-    this.handleCheck = this.handleCheck.bind(this);
-    this.handleCreateNewRoutine = this.handleCreateNewRoutine.bind(this);
+      routineType: '',
+      hapticCheckbox: true
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCreateNewRoutine = this.handleCreateNewRoutine.bind(this)
   }
-  handleCheck = event => {
+
+  // handleChange(key, value) {
+  //   this.setState({[key]: value})
+  //   if (key === 'routineType' && value !== 'combo') {
+  //     const newRoutine = this.state.routine.map(interval => ({
+  //       cadence: interval.cadence,
+  //       duration: interval.duration,
+  //       intervalType: value
+  //     }))
+  //     this.setState({intervalType: value, routine: newRoutine})
+  //   }
+  // }
+
+  handleChange(value) {
     this.setState({
-      hapticCheckBox: event.target.hapticCheckBox,
-    });
-  };
-  onValueChange(value) {
-    this.setState({
-      selectedExerciseType: value,
-    });
+      routineType: value
+    })
   }
   handleCreateNewRoutine() {
     //atm it's not sending the selected exercise along with the created routine.
     //this.props.createRoutineThunk();
-    this.props.navigation.navigate('BuildRoutineScreen');
+    this.props.navigation.navigate('BuildRoutineScreen')
     this.setState({
-      selectedExerciseType: '',
-    });
+      routineType: ''
+    })
   }
 
   static navigationOptions = {
-    header:null
+    header: null
   }
 
   render() {
+    const activityTypeSelects = Object.keys(activityTypes).map(activity => ({
+      label: `${activityTypes[activity].icon} ${activityTypes[activity].display}`,
+      value: activity
+    }))
     return (
       <Container>
         <Content>
           <Form>
             <RNPickerSelect
-              onValueChange={value => this.onValueChange(value)}
-              items={[
-                { label: 'Walking', value: 'walking' },
-                { label: 'Running', value: 'running' },
-                { label: 'Cycling', value: 'cycling' },
-                { label: 'Rowing', value: 'rowing' },
-                { label: 'Jumping Jacks', value: 'jumping jacks' },
-                { label: 'Pushups', value: 'pushups' },
-                { label: 'Breathing', value: 'breathing' },
-                { label: 'Dancing', value: 'dancing' },
-                { label: 'Playing Music', value: 'playing music' },
-              ]}
+              onValueChange={value => this.handleChange(value)}
+              style={{display: 'flex', alignItems: 'center'}}
+              value={this.state.routineType}
+              items={[{label: 'Combo', value: 'combo'}, ...activityTypeSelects]}
             />
           </Form>
           <ListItem>
             <CheckBox
-              onPress={this.handleCheck}
-              checked={this.state.hapticCheckBox}
+              onClick={() =>
+                this.setState(prevState => ({
+                  hapticCheckBox: !prevState.hapticCheckBox
+                }))
+              }
+              isChecked={this.state.hapticCheckBox}
             />
             <Body>
               <Text>Use Default Haptic Settings</Text>
@@ -99,50 +107,96 @@ class SelectRoutineScreen extends Component {
           <Button
             bordered
             style={styles.button}
-            onPress={() => this.props.navigation.navigate('BuildRoutineScreen')}
+            onPress={() => this.handleCreateNewRoutine()}
           >
             <Text>Create New Routine</Text>
           </Button>
-          <Button bordered style={styles.button}>
-            <PreviousRoutine
-              selectedExerciseType={this.state.selectedExerciseType}
-            />
+          <Button
+            onPress={() => this.props.navigation.navigate('PreviousRoutine')}
+            bordered
+            style={styles.button}
+          >
+            <Text>Select A Previous Routine</Text>
+            {/* <PreviousRoutine
+            //routineType={this.state.routineType}
+            /> */}
           </Button>
         </Content>
       </Container>
-    );
+    )
   }
 }
 
 SelectRoutineScreen.navigationOptions = {
   // title:'Select Exercise',
-  header:null
+  header: null
 }
 const styles = StyleSheet.create({
   header: {
-    marginTop: 15,
     fontSize: 20,
     textAlign: 'center',
+    width: '100%',
     color: 'rgba(255,255,255, 0.9)',
+    backgroundColor: 'gray'
+  },
+  buttons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
   button: {
+    //width: '30%',
     margin: 5,
-    padding: 5,
+    padding: 2,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 5
   },
-});
+  buttonText: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white'
+  },
+  message: {
+    fontSize: 10,
+    textAlign: 'center'
+  },
+  item: {
+    height: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'space-between'
+  },
+  checkBox: {
+    height: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  name: {
+    textAlign: 'right'
+  },
+  sectionHeader: {
+    width: '100%',
+    backgroundColor: 'blue',
+    color: 'white',
+    textAlign: 'center'
+  },
+  barGraphic: {
+    marginTop: 20,
+    marginBottom: 20
+  }
+})
 
 const mapStateToProps = state => ({
-  routines: state.routines,
-});
+  routines: state.routines
+})
 
 const mapDispatchToProps = dispatch => ({
   //getRoutineThunk: routineId => dispatch(getRoutineThunk(routineId)),
-  createRoutineThunk: routine => dispatch(createRoutineThunk(routine)),
-});
+  createRoutineThunk: routine => dispatch(createRoutineThunk(routine))
+})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SelectRoutineScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(SelectRoutineScreen)
--- server/api/routines.js
+++ server/api/routines.js
@@ -1,43 +1,51 @@
-const router = require("express").Router();
-const { Routine, Interval } = require("../db/models");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
-const db = require("../db");
+const router = require('express').Router()
+const {Routine, Interval, User, Workout} = require('../db/models')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const db = require('../db')
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const routines = await Routine.findAll({
-      include: [{ model: Interval }, { model: User }, { model: Workout }]
-    });
-    res.json(routines);
+      //include: [{ model: Interval }, { model: User }, { model: Workout }]
+    })
+    res.json(routines)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const {user, body} = req
     const {routineName, routineType, routine, makePublic} = body
     if (user) {
-      console.log("do things");
+      console.log('do things')
     }
     let newRoutine = await Routine.create({
       name: routineName,
       activityType: routineType,
       makePublic
       // userId: user.id
-    });
-    if (!newRoutine) throw new Error("Routine not created");
+    })
+    if (!newRoutine) throw new Error('Routine not created')
     await newRoutine.setIntervals(
-      await Promise.all(routine.map(interval => Interval.create({cadence: interval.cadence, duration: interval.duration, activityType: interval.intervalType})))
-    );
+      await Promise.all(
+        routine.map(interval =>
+          Interval.create({
+            cadence: interval.cadence,
+            duration: interval.duration,
+            activityType: interval.intervalType
+          })
+        )
+      )
+    )
     newRoutine = await Routine.findByPk(newRoutine.id, {include: [Interval]})
     console.log('newRoutine', newRoutine)
-    res.status(201).json(newRoutine);
+    res.status(201).json(newRoutine)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
