22032e4a91ec6668aa813e27e5662c47c281ce04
Ian Roberts <irob466@gmail.com>
2019-11-26 17:37:24 -0600

    added branching path for get class by id if user is leader or follower

--- server/api/classes.js
+++ server/api/classes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router()
-const {Class, Routine, Interval, User} = require('../db/models')
+const {Class, Routine, User} = require('../db/models')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
-const {leaderValidate} = require('./authFunctions')
+const {leaderValidate, authenticatedUser} = require('./authFunctions')
+
+// Quick and dirty testing DO NOT USE IN PRODUCTION
+router.use(async (req, res, next) => {
+  if (process.env.NODE_ENV === 'test') {
+    req.login((await Class.findOne({include: [User]})).user, err =>
+      err ? next(err) : 'good!'
+    )
+  }
+  next()
+})
 
 // GET all classes at /api/class (for populating the class list for search)
 router.get('/', async (req, res, next) => {
@@ -26,25 +36,26 @@ router.get('/', async (req, res, next) => {
 
 // GET single class /api/class/:classId (for populating class data)
 // Currently assuming live class and that attendees and leaders don't need different data
-router.get('/:classId', async (req, res, next) => {
+router.get('/:classId', authenticatedUser, async (req, res, next) => {
   try {
     const {
-      params: {classId}
+      params: {classId},
+      user
     } = req
+    const include = [Routine]
+    if (await user.hasClass(classId))
+      include.push({
+        model: User,
+        as: 'attendees',
+        attributes: ['id', 'email', 'role'],
+        through: {
+          attributes: []
+        }
+      })
     const currentClass = await Class.findByPk(classId, {
       // include age, sex, role of attendees?
       // used to load up the routine/intervals for every user and class list
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'name'],
-          through: {
-            /* I want the attendees not the owner */
-          }
-        },
-        Routine,
-        Interval
-      ]
+      include
     })
     if (!currentClass) throw new Error(`Class with id ${classId} not found.`)
     res.status(200).json(currentClass)
@@ -52,30 +63,5 @@ router.get('/:classId', async (req, res, next) => {
     next(err)
   }
 })
-
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const {
-//       user,
-//       body: {name, activityType, routine}
-//     } = req
-//     console.log(name, activityType, routine)
-//     if (user) {
-//       console.log('do things')
-//     }
-//     const newRoutine = await Routine.create({
-//       name,
-//       activityType
-//       // userId: user.id
-//     })
-//     if (!newRoutine) throw new Error('Routine not created')
-//     await newRoutine.addIntervals(
-//       await Promise.all(routine.map(r => Interval.create(r)))
-//     )
-//     res.status(201).json(newRoutine)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
 
 module.exports = router
