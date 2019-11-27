a9171412e54ce987e85f63b4d153590eedc61db9
Munaiz <munaizahmed2@gmail.com>
2019-11-26 18:42:40 -0600

    added classes reducer. classes APIs were already coded. need to hook up to ClassesScreen.js

---
+++ store/classes.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import {ngrok} from '../ngrok'
+import { debuggerStatement } from '@babel/types';
+
+const GET_CLASSES = 'GET_CLASSES'
+
+const getClasses = classes =>({type:GET_CLASSES,classes})
+
+export const fetchClasses = () => async dispatch =>{
+  try{
+    const allClasses = await axios.get(`${ngrok}/api/classes`)
+    dispatch(getClasses(allClasses.data))
+  } catch (error){
+    console.error(error)
+  }
+}
+
+initialState = []
+
+const classesReducer = (state = initialState, action) => {
+  switch(action.type){
+    case GET_CLASSES:
+      return action.classes
+    default:
+      return state
+  }
+}
+
+export default classesReducer
