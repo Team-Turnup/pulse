c9ac1bd6d6a5d8baa361d3083cd38709042cf232
Ian Roberts <irob466@gmail.com>
2019-11-27 16:16:14 -0600

    socket architechture and basic functionality tested and working

--- socket.js
+++ socket.js
@@ -2,9 +2,10 @@ import io from 'socket.io-client'
 import {ngrok} from './ngrok.js'
 
 const socket = io(ngrok)
-
 socket.on('connect', () => {
   console.log('Connected!')
 })
+
+socket.on('message', message => console.log(message))
 
 export default socket
