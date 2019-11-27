dce3fb6f0939fac0a4c8aad58c1d82f331a72394
Ian Roberts <irob466@gmail.com>
2019-11-27 12:10:42 -0600

    identifying roomleader

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,4 +1,5 @@
 module.exports = io => {
+  let roomLeaders = {}
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
@@ -6,19 +7,29 @@ module.exports = io => {
       console.log(`Connection ${socket.id} has left the building`)
     })
 
-    socket.on('subscribe', roomNum => {
-      console.log(`subscribed to ${roomNum}`)
+    // socket.on('identify', roomNum => {
+    //   roomLeaders.push({[roomNum]: socket.id})
+    // })
+
+    socket.on('subscribe', roomNum, isLeader => {
+      if (isLeader) {
+        console.log('hello leader')
+        roomLeaders[roomNum] = socket.id
+      }
       socket.join(roomNum)
     })
 
-    socket.on('unsubscribe', roomNum => {
-      console.log(`unsubscribed from ${roomNum}`)
+    socket.on('unsubscribe', roomNum, isLeader => {
+      if (isLeader) {
+        console.log('bye leader')
+        delete roomLeaders[roomNum]
+      }
       socket.leave(roomNum)
     })
 
     socket.on('message', (roomNum, message) => {
       console.log(message)
-      socket.to(roomNum).emit('message', message)
+      socket.to(roomLeaders[roomNum]).emit('message', message)
     })
   })
 }
